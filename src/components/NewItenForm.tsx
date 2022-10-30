fc629e817fd4da513ce1aa904d1a3d37b9572952
Ayaf-Bless <blessambel1@gmail.com>
2022-10-31 01:17:07 +0300

    create a hook to focus automatically the input

--- src/components/NewItenForm.tsx
+++ src/components/NewItenForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "../styles";
+import { useFocus } from "../utils/useFocus";
 
 interface NewItemFormProps {
   onAdd(text: string): void;
@@ -7,10 +8,15 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
+  const inputRef = useFocus();
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <NewItemButton onClick={() => onAdd(text)}>create</NewItemButton>
     </NewItemFormContainer>
   );
