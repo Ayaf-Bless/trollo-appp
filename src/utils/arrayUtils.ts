f8d4537a305005113896c1674e83725d587d59df
Ayaf-Bless <blessambel1@gmail.com>
2022-11-18 18:40:22 +0300

    implement move list

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -5,3 +5,17 @@ type Item = {
 export const findItemIndexById = <T extends Item>(item: T[], id: string) => {
   return item.findIndex((item: T) => item.id === id);
 };
+
+export const removeItemAtIndex = <T>(array: T[], index: number) => {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+};
+
+export const insertIndexAtIndex = <T>(array: T[], item: T, index: number) => {
+  return [...array.slice(0, index), item, ...array.slice(index)];
+};
+
+export const moveItem = <T>(array: T[], from: number, to: number) => {
+  const item = array[from];
+
+  return insertIndexAtIndex(removeItemAtIndex(array, from), item, to);
+};
